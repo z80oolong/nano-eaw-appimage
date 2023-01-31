dcc3b43289544bac7e1dc66465892d4093eed9d6
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2023-01-31 20:37:15 +0900

    Update about libc.so.6 problem, for building `nano-{4.9.1-7.1}-x86_64.AppImage`.

--- lib/config.rb
+++ lib/config.rb
@@ -2,7 +2,17 @@ module Config
   module_function
 
   def stable_version?
-    return false
+    return true
+  end
+
+  def current_vm_provider
+    return "lxc"
+    #return "libvirt"
+  end
+
+  def current_libvirt_driver
+    return "qemu"
+    #return "kvm"
   end
 
   def appimage_tap_name
@@ -25,20 +35,20 @@ module Config
     return "#{current_formula_name}-eaw"
   end
 
-  def current_version
+  def current_version_list
     if stable_version? then
-      return "6.4"
+      return %w[4.9.2 4.9.3 5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.6.1 5.7 5.8 5.9 6.0 6.1 6.2 6.3 6.4 7.0 7.1]
     else
-      return "HEAD-#{commit}"
+      return ["HEAD-#{commit}"]
     end
   end
 
   def all_stable_version
-    return %w[4.9.2 4.9.3 5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.6.1 5.7 5.8 5.9 6.0 6.1 6.2 6.3 6.4]
+    return %w[4.9.2 4.9.3 5.0 5.1 5.2 5.3 5.4 5.5 5.6 5.6.1 5.7 5.8 5.9 6.0 6.1 6.2 6.3 6.4 7.0 7.1 7.2]
   end
 
   def current_head_formula_version
-    return "7.0-next"
+    return "8.0-next"
   end
 
   def all_stable_formulae
@@ -52,7 +62,7 @@ module Config
   end
 
   def commit_long
-    return "e413ed8e1d8a60e44f07dd8d8080d783ce009039"
+    return "1c307bc1be2906f10f7a4430f7603a3c620fb064"
   end
 
   def commit
@@ -60,7 +70,7 @@ module Config
   end
 
   def current_appimage_revision
-    return 50
+    return 51
   end
 
   def release_dir
